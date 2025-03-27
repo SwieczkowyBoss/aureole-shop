51d6d0ff6aa92a7f6cb519c73f1b02dbb91f8b4d
SwieczkowyBoss <xkokonatx@gmail.com>
2025-03-27 15:10:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementById('notify-form').addEventListener('submit', function(e) {
   e.preventDefault();
   document.getElementById('notify-confirmation').classList.remove('hidden');
 });
+
+document.getElementById('close-overlay').addEventListener('click', function () {
+  document.getElementById('overlay').style.display = 'none';
+});
