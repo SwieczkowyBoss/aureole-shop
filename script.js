9c7fb20ab91329431e28fcb0a950fb109496f4d4
SwieczkowyBoss <xkokonatx@gmail.com>
2025-03-27 14:56:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-document.getElementById('close-overlay').addEventListener('click', () => {
-  document.getElementById('overlay').style.display = 'none';
-  localStorage.setItem('overlayClosed', 'true');
-});
-
-window.addEventListener('load', () => {
-  if (localStorage.getItem('overlayClosed') === 'true') {
-    document.getElementById('overlay').style.display = 'none';
-  }
-});
-
-document.getElementById('notify-form').addEventListener('submit', (e) => {
+document.getElementById('notify-form').addEventListener('submit', function(e) {
   e.preventDefault();
-  const emailInput = e.target.querySelector('input[type="email"]');
-  const confirmation = document.getElementById('notify-confirmation');
-  const email = emailInput.value;
-
-  // symulacja zapisu (docelowo: backend/API)
-  console.log("Zapisano e-mail:", email);
-  confirmation.classList.remove('hidden');
-  emailInput.value = '';
+  document.getElementById('notify-confirmation').classList.remove('hidden');
 });
